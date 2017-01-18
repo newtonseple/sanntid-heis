5302c1e062dbb84cc96e7c29d3f5ec82c2e78ba0
Unknown <newtonseple@gmail.com>
2017-01-18 15:30:48 +0100

    Commit

--- Exercises/ex2/ex2.go
+++ Exercises/ex2/ex2.go
@@ -9,15 +9,20 @@ import (
     "time"
 )
 
-var i int = 0
-func thread_1_func() {
+
+func thread_1_func(i_glob chan int) {
     for n := 0 ; n < 1000000 ; n++{
-        i++;
+        i := <- i_glob
+        i++
+        i_glob <- i
     }
 }
-func thread_2_func() {
+func thread_2_func(i_glob chan int) {
+    var i int = 0
     for n := 0 ; n < 1000000 ; n++{
-        i--;
+        i := <- i_glob
+        i--
+        i_glob <- i
     }
 }
 
@@ -25,8 +30,11 @@ func thread_2_func() {
 func main() {
     runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
                                             // Try doing the exercise both with and without it!
-    go thread_1_func()                      // This spawns someGoroutine() as a goroutine
-    go thread_2_func() 
+    i_glob := make(chan int)
+
+    go thread_1_func(i_glob)                      // This spawns someGoroutine() as a goroutine
+    go thread_2_func(i_glob) 
+
     // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
     // We'll come back to using channels in Exercise 2. For now: Sleep.
     time.Sleep(100*time.Millisecond)
