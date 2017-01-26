b2e55237fe77514e199de0867b835114d3e14d81
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-01-26 15:37:01 +0100

    Add bounds check in driver. TODO: test

--- manhattan/src/driver/mod.rs
+++ manhattan/src/driver/mod.rs
@@ -2,47 +2,46 @@ mod c_driver;
 
 pub use self::c_driver::elev_button_type_t;
 pub use self::c_driver::elev_motor_direction_t;
-pub use self::c_driver::n_floors; // Is this needed?
+pub use self::c_driver::n_floors; // Is this needed? As pub?
 
 pub fn init() {
-    unsafe {
-        c_driver::elev_init();
-    }
+    unsafe { c_driver::elev_init(); }
 }
 
 pub fn set_motor_direction(direction: elev_motor_direction_t) {
-    unsafe {
-        c_driver::elev_set_motor_direction(direction);
-    }
+    unsafe { c_driver::elev_set_motor_direction(direction); }
 }
 
 pub fn set_button_lamp(button: elev_button_type_t, floor: i32, value: bool) {
-    unsafe {
-        c_driver::elev_set_button_lamp(button, floor, value as i32);
-    }
+    match floor {
+		0..n_floors => unsafe { c_driver::elev_set_button_lamp(button, floor, value as i32); },
+		_ => panic!("Tried to set button lamp in non-existing floor!");
+	}    
 }
 
 pub fn set_floor_indicator(floor: i32) {
-    unsafe {
-        c_driver::elev_set_floor_indicator(floor);
-    }
+	match floor {
+		0..n_floors => unsafe { c_driver::elev_set_floor_indicator(floor); },
+		_ => panic!("Tried to set the floor indicator in a nonexisting floor!");
+	}
+    
 }
 
 pub fn set_door_open_lamp(value: bool) {
-    unsafe {
-        c_driver::elev_set_door_open_lamp(value as i32);
-    }
+    unsafe { c_driver::elev_set_door_open_lamp(value as i32); }
 }
 
 pub fn set_stop_lamp(value: bool) {
-    unsafe {
-        c_driver::elev_set_stop_lamp(value as i32);
-    }
+    unsafe { c_driver::elev_set_stop_lamp(value as i32); }
 }
 
 
 pub fn get_button_signal(button: elev_button_type_t, floor: i32) -> bool {
-    unsafe { c_driver::elev_get_button_signal(button, floor) != 0 }
+	match floor {
+		0..n_floors => unsafe { c_driver::elev_get_button_signal(button, floor) != 0 },
+		_ => panic!("Tried to get a button signal in a nonexisting floor");
+	}
+    
 }
 
 // returns the floor (0-indexed) or -1 if the carriage is between floors
@@ -60,8 +59,6 @@ pub fn get_obstruction_signal() -> bool {
 
 
 pub fn test_run() -> ! {
-    unsafe {
-        c_driver::test_run();
-    }
+    unsafe { c_driver::test_run(); }
     loop {}
 }
