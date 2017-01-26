eabe66a95193de4eda053b84f36c8366e1eaf6fa
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-01-26 13:23:47 +0100

    Complete C interface

--- manhattan/src/driver/mod.rs
+++ manhattan/src/driver/mod.rs
@@ -1,25 +1,67 @@
 mod c_driver;
-pub use self::c_driver::elev_button_type_t;
 
-pub fn go_up() {
-	println!("going up");
+pub use self::c_driver::elev_button_type_t;
+pub use self::c_driver::elev_motor_direction_t;
+pub use self::c_driver::n_floors; // Is this needed?
+
+pub fn init() {
+    unsafe {
+        c_driver::elev_init();
+    }
 }
-pub fn test_run() -> ! {
-	unsafe { c_driver::test_run(); }
-	loop {};
+
+pub fn set_motor_direction(direction: elev_motor_direction_t) {
+    unsafe {
+        c_driver::elev_set_motor_direction(direction);
+    }
 }
-pub fn set_button_lamp(button: elev_button_type_t , floor: i32, value: i32){
-	unsafe { c_driver::elev_set_button_lamp(button, floor, value); }
+
+pub fn set_button_lamp(button: elev_button_type_t, floor: i32, value: bool) {
+    unsafe {
+        c_driver::elev_set_button_lamp(button, floor, value as i32);
+    }
 }
 
 pub fn set_floor_indicator(floor: i32) {
-	unsafe { c_driver::elev_set_floor_indicator(floor); }
+    unsafe {
+        c_driver::elev_set_floor_indicator(floor);
+    }
 }
 
-pub fn init() {
-	unsafe { c_driver::elev_init(); }
+pub fn set_door_open_lamp(value: bool) {
+    unsafe {
+        c_driver::elev_set_door_open_lamp(value as i32);
+    }
+}
+
+pub fn set_stop_lamp(value: bool) {
+    unsafe {
+        c_driver::elev_set_stop_lamp(value as i32);
+    }
+}
+
+
+pub fn get_button_signal(button: elev_button_type_t, floor: i32) -> bool {
+    unsafe { c_driver::elev_get_button_signal(button, floor) != 0 }
+}
+
+// returns the floor (0-indexed) or -1 if the carriage is between floors
+pub fn get_floor_sensor_signal() -> i32 {
+    unsafe { c_driver::elev_get_floor_sensor_signal() }
+}
+
+pub fn get_stop_signal() -> bool {
+    unsafe { c_driver::elev_get_stop_signal() != 0 }
 }
 
 pub fn get_obstruction_signal() -> bool {
-	unsafe { c_driver::elev_get_obstruction_signal() != 0 }
+    unsafe { c_driver::elev_get_obstruction_signal() != 0 }
+}
+
+
+pub fn test_run() -> ! {
+    unsafe {
+        c_driver::test_run();
+    }
+    loop {}
 }
