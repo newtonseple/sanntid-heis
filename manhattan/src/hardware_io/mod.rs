6f590eb62a6ef85861d237748799d045d0d5ddc1
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-02-01 23:34:26 +0100

    Add beginning of hardware polling

--- manhattan/src/hardware_io/mod.rs
+++ manhattan/src/hardware_io/mod.rs
@@ -1,11 +1,17 @@
+// External dependencies
 use std::sync::mpsc;
 use std::thread;
 
-mod rust_driver;
+mod local_controller;
+mod planner;
 
+// Internal dependencies
+mod rust_driver;
 use self::rust_driver::*;
+
 pub use self::rust_driver::elev_button_type_t;
 pub use self::rust_driver::elev_motor_direction_t;
+
 
 pub enum HwCommandMessage {
     SetButtonLamp {
@@ -17,22 +23,31 @@ pub enum HwCommandMessage {
     SetMotorDirection { direction: elev_motor_direction_t },
 }
 
-pub fn start(hw_command_rx: mpsc::Receiver<HwCommandMessage>) -> thread::JoinHandle<()> {
+
+pub fn start(local_event_tx: mpsc::Sender<local_corntroller::LocalEventMessage>, add_order_tx: mpsc::channel<planner::Order>, hw_command_rx: mpsc::Receiver<HwCommandMessage>) -> thread::JoinHandle<()> {
     thread::spawn(move || {
         init();
         loop {
             if let Ok(command) = hw_command_rx.try_recv() {
                 match command {
-                    HwCommandMessage::SetButtonLamp { button_type, floor, value } => {
-                        set_button_lamp(button_type, floor, value)
-                    }
+                    HwCommandMessage::SetButtonLamp { button_type, floor, value } => set_button_lamp(button_type, floor, value),
                     HwCommandMessage::SetDoorOpenLamp { value } => set_door_open_lamp(value),
-                    HwCommandMessage::SetMotorDirection { direction } => {
-                        set_motor_direction(direction)
-                    }
+                    HwCommandMessage::SetMotorDirection { direction } => set_motor_direction(direction),
                 }
             }
-            //Autonomous behavour goes here!!
+            
+            // Poll all hardware inputs
+
+            let floor_sensor_result = get_floor_sensor_signal();
+            if floor_sensor_result != -1 {
+                local_event_tx.send(local_controller::LocalEventMessage::ArrivedAtFloor{ floor = floor_sensor_result };
+            }
+
+            for floor in 0..N_FLOORS {
+                if get_button_signal(elev_button_type_t::UP, floor) {
+
+                }
+            }
         }
 
     })
---
+++ manhattan/src/planner/mod.rs
@@ -0,0 +1,6 @@
+mod driver;
+
+pub struct Order {
+    floor: i32,
+    type: driver::elev_button_type_t,
+}
