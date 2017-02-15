9f394027d26fba3121725ce7a905a953b62900b4
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-02-15 20:46:34 +0100

    Implemented timer

---
+++ manhattan/src/timer/mod.rs
@@ -0,0 +1,14 @@
+use std::thread::sleep;
+use std::sync::mpsc::Sender;
+use std::time::Duration;
+
+use local_controller;
+
+pub fn start(timer_tx: Sender) {
+    thread::spawn(move || {
+        loop {
+            timer_tx.send(local_controller::LocalEventMessage::TimerTick)
+            sleep(Duration::from_millis(100));
+        }
+    })
+}
