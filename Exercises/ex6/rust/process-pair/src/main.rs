9ebf16232f786eb488b7697511c1ffd8eeb15197
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-02-21 19:02:35 +0100

    Did some work

--- Exercises/ex6/rust/process-pair/src/main.rs
+++ Exercises/ex6/rust/process-pair/src/main.rs
@@ -1,6 +1,23 @@
 use std::process::Command;
+use std::time;
+use std::thread;
+use std::env;
 
 fn main() {
-    let output = Command::new("echo").arg("Hello").output().expect("Failed to execute command");
-    println!("{:?}", output);
+    let arg = env::args().nth(1).expect("Did not get a argument");
+    println!("{:?}", arg);
+    let mut i: i32 = arg.parse().unwrap();
+    i += 1;
+    if i < 10 {
+        let output = Command::new("start")
+            .arg("call")
+            .arg("target\\debug\\process-pair")
+            .arg(format!("{}", i))
+            .spawn()
+        //    .expect("Failed to spawn process");
+        .expect("Failed to execute command");
+    } else {
+        println!("This is the end: {}", i);
+    }
+    thread::sleep(time::Duration::from_secs(10));
 }
