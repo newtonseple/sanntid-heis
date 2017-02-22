ef6e8db486609e56c0c17c3deb0333df1476c468
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-02-22 16:47:24 +0100

    did some more work

--- Exercises/ex6/rust/process-pair/src/main.rs
+++ Exercises/ex6/rust/process-pair/src/main.rs
@@ -2,8 +2,19 @@ use std::process::Command;
 use std::time;
 use std::thread;
 use std::env;
+use std::fs;
+
+fn get_state() -> (i32, time::Instant) {
+    fs::File::create("test");
+    for file in fs::read_dir(".") {
+        println!("{:?}", file);
+    }
+    return (0, time::Instant::now());
+}
 
 fn main() {
+    get_state();
+    /*
     let arg = env::args().nth(1).expect("Did not get a argument");
     println!("{:?}", arg);
     let mut i: i32 = arg.parse().unwrap();
@@ -20,4 +31,5 @@ fn main() {
         println!("This is the end: {}", i);
     }
     thread::sleep(time::Duration::from_secs(10));
+    */
 }
