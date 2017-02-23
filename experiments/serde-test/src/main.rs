b57645b2d674aced19b66c4384e6f3b0ad098f35
Didrik Rokhaug <didrik.rokhaug@gmail.com>
2017-02-23 13:00:48 +0100

    Add serde experiment

---
+++ experiments/serde-test/src/main.rs
@@ -0,0 +1,53 @@
+extern crate serde_json;
+extern crate serde;
+
+#[macro_use]
+extern crate serde_derive;
+
+use serde::Serialize;
+use serde::Deserialize;
+
+#[derive(Serialize)]
+#[derive(Deserialize)]
+#[derive(Debug)]
+enum SomeEnum {
+    Foo,
+    Bar(i32),
+    Baz { a: i32, b: bool },
+    Boo { c: SomeStruct },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct SomeStruct {
+    a: i32,
+    b: bool,
+}
+
+
+
+fn main() {
+    let a = SomeEnum::Foo;
+    let b = SomeEnum::Bar(42);
+    let c = SomeEnum::Baz { a: 42, b: true };
+    let d = SomeEnum::Boo { c: SomeStruct { a: 24, b: false } };
+    let aj = serde_json::to_string(&a).unwrap();
+    let bj = serde_json::to_string(&b).unwrap();
+    let cj = serde_json::to_string(&c).unwrap();
+    let dj = serde_json::to_string(&d).unwrap();
+    let ad: Result<SomeEnum, _> = serde_json::from_str(&aj);
+    let bd: Result<SomeEnum, _> = serde_json::from_str(&bj);
+    let cd: Result<SomeEnum, _> = serde_json::from_str(&cj);
+    let dd: Result<SomeEnum, _> = serde_json::from_str(&dj);
+    println!("a: {:?}", a);
+    println!("b: {:?}", b);
+    println!("c: {:?}", c);
+    println!("d: {:?}", d);
+    println!("aj: {:?}", aj);
+    println!("bj: {:?}", bj);
+    println!("cj: {:?}", cj);
+    println!("dj: {:?}", dj);
+    println!("ad: {:?}", ad);
+    println!("bd: {:?}", bd);
+    println!("cd: {:?}", cd);
+    println!("dd: {:?}", dd);
+}
