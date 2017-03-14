992ebfe1af4a16cfa0c7559dbcd458cc2395c04e
Unknown <newtonseple@gmail.com>
2017-03-15 00:31:49 +0100

    Beautiful planner?

--- manhattan/src/planner/mod.rs
+++ manhattan/src/planner/mod.rs
@@ -101,31 +101,28 @@ pub fn run(hw_command_tx: mpsc::Sender<hardware_io::HwCommandMessage>,
                         }
                     }
 
-                    // If we loose a peer, their orders will be redelegated
                     for id in peer_update.lost {
                         elevator_data_map.get_mut(&id).unwrap().alive = false;
                         println!("Peer died: {}", id);
-                        
+
+                        // The hall orders of the lost peer will be redelegated
                         for order in (*elevator_data_map.get(&id).unwrap()).get_orders().iter()
                             .filter(|&order| order.order_type != OrderType::CAB)
                         {
                             add_order_tx.send(*order).expect("Failed to queue order for new assignment");
                         }
                     }
-                    
                 },
 
-
+                // All network message handling happens here
                 message_recieved_result = message_recieved_rx.recv() => {
                     let message_recieved = message_recieved_result.expect("message_recieved_result failed");
-                    //println!("Got net message! {:?}", message_recieved);
                     let local_ip = network::get_localip()
-                        .expect("Could not get local ip 99999783");
+                        .expect("Could not get local ip");
                     match message_recieved.data {
                         network::SendMessageCommand::NewOrder{order_type, floor, id} => {
-                            //println!("New order from network");
-                            
-                            //Make sure the entry exists in the order table
+
+                            //Make sure the elevator exists in the map
                             if ! (elevator_data_map.contains_key(&id)){
                                 elevator_data_map.insert(id.clone(), ElevatorData::new());
                                 println!("Added new elevator_data for: {}", id);
@@ -134,23 +131,23 @@ pub fn run(hw_command_tx: mpsc::Sender<hardware_io::HwCommandMessage>,
                             elevator_data_map.get_mut(&id)
                                 .expect(format!("ID not in map: {}",id).as_str())
                                 .set_order(order_type, floor, true);
-                            if id == local_ip || order_type == OrderType::UP || order_type == OrderType::DOWN{
-                                hw_command_tx.send(HwCommandMessage::SetButtonLamp{
+                            if id == local_ip || order_type == OrderType::UP || order_type == OrderType::DOWN {
+                                hw_command_tx.send(HwCommandMessage::SetButtonLamp {
                                     button_type: order_type,
                                     floor: floor,
                                     value: true,
-                                }).expect("Could not send light message 3276487");
+                                }).expect("Could not send light message");
                             }
                         },
+
                         network::SendMessageCommand::StateUpdate{direction, floor} => {
-                            //println!("State upd rcv");
                             match elevator_data_map.get_mut(&message_recieved.id){
                                 Some(elevator_data) => elevator_data.update_state(floor, direction),
-                                None => {}//println!("No elevator data for remote ip")
+                                None => {}
                             }
                         }
+
                         network::SendMessageCommand::OrderComplete{order_type, floor} => {
-                            //println!("Order complete");
                             for (id, elevator_data) in elevator_data_map.iter_mut() {
                                 elevator_data.set_order(order_type, floor, false);
                                 if order_type == OrderType::UP || order_type == OrderType::DOWN {
@@ -158,7 +155,7 @@ pub fn run(hw_command_tx: mpsc::Sender<hardware_io::HwCommandMessage>,
                                         button_type: order_type,
                                         floor: floor,
                                         value: false,
-                                    }).expect("Could not send light message 32764");
+                                    }).expect("Could not send light message");
                                 }
                                 if *id == message_recieved.id {
                                     elevator_data.set_order(OrderType::CAB, floor, false);
@@ -174,11 +171,13 @@ pub fn run(hw_command_tx: mpsc::Sender<hardware_io::HwCommandMessage>,
                         }
                     }
                 },
+
+                // This is the response to the local controller asking what to do,
+                // provided by the ElevatorData representing the local elevator.
                 local_command_request_result = local_command_request_rx.recv() => {
-                    //println!("got local command request");
                     let local_command_request = local_command_request_result.expect("local_command_request_result failed");
                     let local_ip = network::get_localip()
-                        .expect("Could not get local ip 9999978");
+                        .expect("Could not get local ip");
                     let local_elevator_data_option = elevator_data_map
                         .get_mut(&local_ip);
                     match local_elevator_data_option{
@@ -186,14 +185,13 @@ pub fn run(hw_command_tx: mpsc::Sender<hardware_io::HwCommandMessage>,
                             local_elevator_data.update_state(local_command_request.floor, local_command_request.current_service_direction);
                             let local_command = local_elevator_data.get_local_command();
                             local_command_tx.send(local_command)
-                                .expect("Could not send local command 66668234");
+                                .expect("Could not send local command");
                         }
                         None => {
                             local_command_tx.send(local_controller::LocalCommandMessage::DoNothing)
-                                .expect("Could not send local command 68234");
+                                .expect("Could not send local command");
                         } 
                     }
-                    //println!("sent local command");
                 }
             }
         }
